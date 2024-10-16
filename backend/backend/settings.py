074bb8cd3c474f0adb66b77bfece8ad1772ee812
PranjalDas <pranjal080015@gmail.com>
2024-10-16 14:41:55 +0530

    s

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,6 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
+]
